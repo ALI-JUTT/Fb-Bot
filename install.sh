bcdc1e698af831f68429bb136f1dc209bec52882
Ali jutt <82082476+ALI-JUTT@users.noreply.github.com>
2024-09-01 16:54:30 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!system/bin/sh
+
+pkg update && pkg upgrade -y -y
+pkg install root-repo -y
+pkg install unstable-repo -y
+pkg install x11-repo -y
+pkg install python -y
+termux-setup-storage
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python run.py
